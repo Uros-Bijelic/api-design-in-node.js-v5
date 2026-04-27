1b65db23833dce298dd306d2067904dda9df6699
urke90 <urosbijelic90@gmail.com>
2026-04-27 20:13:54 +0200

    add relations for tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,6 +8,7 @@ import {
     boolean,
 } from 'drizzle-orm/pg-core';
 import { relations } from 'drizzle-orm';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
 export const users = pgTable('users', {
     id: uuid('id').primaryKey().defaultRandom(),
@@ -52,13 +53,57 @@ export const tags = pgTable('tags', {
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
 
-export const habitTage = pgTable('habitTags', {
+export const habitTags = pgTable('habitTags', {
     id: uuid('id').primaryKey().defaultRandom(),
     habitId: uuid('habit_id')
         .references(() => habits.id, { onDelete: 'cascade' })
         .notNull(),
-    tagId: uuid('habit_id')
+    tagId: uuid('tag_id')
         .references(() => tags.id, { onDelete: 'cascade' })
         .notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
 });
+
+export const userRelations = relations(users, ({ many }) => ({
+    habits: many(habits),
+}));
+
+export const habitRelations = relations(habits, ({ one, many }) => ({
+    user: one(users, {
+        fields: [habits.userId],
+        references: [users.id],
+    }),
+    entries: many(entries),
+    habitTags: many(habitTags),
+}));
+
+export const entriesRelations = relations(entries, ({ one }) => ({
+    habit: one(habits, {
+        fields: [entries.habitId],
+        references: [habits.id],
+    }),
+}));
+
+export const tagsRealtions = relations(tags, ({ many }) => ({
+    habitTags: many(habitTags),
+}));
+
+export const habitTagsRelations = relations(habitTags, ({ one }) => ({
+    habit: one(habits, {
+        fields: [habitTags.habitId],
+        references: [habits.id],
+    }),
+    tag: one(tags, {
+        fields: [habitTags.tagId],
+        references: [tags.id],
+    }),
+}));
+
+export type User = typeof users.$inferSelect;
+export type Habit = typeof habits.$inferSelect;
+export type Entry = typeof entries.$inferSelect;
+export type Tag = typeof tags.$inferSelect;
+export type HabitTag = typeof habitTags.$inferSelect;
+
+export const insertUserSchema = createInsertSchema(users);
+export const selectUserSchema = createSelectSchema(users);
